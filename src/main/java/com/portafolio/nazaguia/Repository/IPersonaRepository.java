ac3e61d221f5e9da9cc0214d05aa8a205615790d
nazapws <nazguiag@gmail.com>
2023-04-27 14:20:09 -0300

    ahorasi

---
+++ src/main/java/com/portafolio/nazaguia/Repository/IPersonaRepository.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.portafolio.nazaguia.Repository;
+
+/**
+ *
+ * @author Administrator
+ */
+import com.portafolio.nazaguia.Entity.Persona;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPersonaRepository extends JpaRepository <Persona, Integer>{
+ 
+    public Optional<Persona> findByNombre (String nombre);
+    public boolean existsByNombre(String nombre);
+}
