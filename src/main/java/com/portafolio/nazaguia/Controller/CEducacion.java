ed68739d43a097cc0b97a0a9fb5327049db4fb55
nazapws <nazguiag@gmail.com>
2023-05-03 15:39:35 -0300

    deploy1

--- src/main/java/com/portafolio/nazaguia/Controller/CEducacion.java
+++ src/main/java/com/portafolio/nazaguia/Controller/CEducacion.java
@@ -22,7 +22,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://front-4f595.web.app")
 public class CEducacion {
     @Autowired
     Seducacion sEducacion;
--- src/main/java/com/portafolio/nazaguia/Controller/PersonaController.java
+++ src/main/java/com/portafolio/nazaguia/Controller/PersonaController.java
@@ -32,7 +32,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/personas")
-@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://front-4f595.web.app")
 public class PersonaController {
     @Autowired
     ImpPersonaService personaService;
