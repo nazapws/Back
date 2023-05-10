3eb58d03770e05bce35c96d0ceccc146ef2580b0
nazapws <nazguiag@gmail.com>
2023-05-10 01:03:56 -0300

    deploy3.1

--- src/main/java/com/portafolio/nazaguia/Controller/PersonaController.java
+++ src/main/java/com/portafolio/nazaguia/Controller/PersonaController.java
@@ -32,7 +32,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/personas")
-@CrossOrigin(origins = {"http://localhost:4200", "https://front-4f595.web.app"})
+//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://front-4f595.web.app")
 public class PersonaController {
     @Autowired
     ImpPersonaService personaService;
@@ -67,7 +68,7 @@ public ResponseEntity<?> update(@PathVariable("id") int id, @RequestBody dtoPers
     Persona persona = personaService.getOne(id).get();
     
     persona.setNombre(dtopersona.getNombre());
-    persona.setApellido(dtopersona.getApellido());
+    persona.setDescripcion(dtopersona.getDescripcion());
     persona.setDescripcion(dtopersona.getDescripcion());
     persona.setImg(dtopersona.getImg());
     
--- src/main/java/com/portafolio/nazaguia/Security/Controller/AuthController.java
+++ src/main/java/com/portafolio/nazaguia/Security/Controller/AuthController.java
@@ -38,7 +38,8 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"http://localhost:4200", "https://front-4f595.web.app"})
+@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "http://localhost:4200")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
